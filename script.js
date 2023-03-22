7b6189d6d293d6194d8255aed20b14186569bfc4
yafox3 <ruslan.kret@yandex.ru>
2023-03-22 17:31:41 +0300

    v1.0

---
+++ script.js
@@ -0,0 +1,20 @@
+const API_URL = 'https://api.shrtco.de/v2/shorten?url='
+const generateBtn = document.querySelector('#generate')
+
+generateBtn.addEventListener('click', () => {
+    const input = document.querySelector('#input')
+    getShort(`${input.value}`).then(val => createResult(val.result.short_link))
+    input.value = ''
+})
+
+function getShort(link) {
+    return fetch(API_URL + link).then(response => response.json())
+}
+
+function createResult(link) {
+    const resultEl = document.querySelector('#result')
+    resultEl.innerHTML = link
+    resultEl.setAttribute('href', link)
+}
+
+
